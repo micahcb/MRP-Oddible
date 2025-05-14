c7bce7e081db6259d273131053bb2023020c903f
micahcb <micahcb@umich.edu>
2025-05-14 18:20:34 -0400

    ready for sharing

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,5 +1,17 @@
-import { Slot } from "expo-router";
+import { Slot } from 'expo-router';
+import { Auth0Provider } from 'react-native-auth0';
+import StripeProvider from '../components/StripeProvider';
 
 export default function RootLayout() {
-  return <Slot />;
+
+    return (
+      <Auth0Provider 
+        domain="auth.smartbettor.ai" 
+        clientId={process.env.EXPO_PUBLIC_AUTH0_CLIENT_ID}
+      >
+        <StripeProvider>
+          <Slot />
+        </StripeProvider>
+      </Auth0Provider>
+    );
 }
